0c93b921e47b8feb899a0fb789e55af7eee71f06
ericloux <eric.charles.loux@gmail.com>
2019-06-23 12:22:29 -0500

    Added game to projects

---
+++ components/navigation.js
@@ -0,0 +1,8 @@
+export default `
+<p class="navTitle">NAVIGATION</p>
+<ul>
+    <li class="navItem"><a href="../"><i class="fas fa-home"></i>HOME</a></li>
+    <li class="navItem"><a href="/projects/index.html"><i class="fas fa-laptop-code"></i>PROJECTS</a></li>
+    <li class="navItem"><a href="/contact/index.html"><i class="far fa-envelope-open"></i>CONTACT</a></li>
+</ul>
+`;
---
+++ index.js
@@ -0,0 +1,25 @@
+//import Navigation from "./components/navigation.js";
+
+//console.log(Navigation); // just to test that our import is working
+
+function multiply(first, second) {
+    return first*second;
+}
+
+function tripler(number) {
+    return multiply(number,3);
+}
+
+function divide(first, second) {
+    return first/second;
+}
+
+function remainder(first, second) {
+    return first%second;
+}
+
+function looper() {
+    for (i=1; i<21; i++) {
+        console.log(i);
+    }
+}
